2187cb1ac642ca9cb73fdfcb47198188b1a5ff9b
Mel66292 <mel57792@yandex.ru>
2023-07-18 16:19:43 +0500

    -First commit

---
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -0,0 +1,9 @@
+package ru.netology.service;
+
+public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
+}
