32fe758524543db1dcd9b2e4a7d0a4588a72cbe2
Mel66292 <mel57792@yandex.ru>
2023-07-19 15:48:40 +0500

    junit5 added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,7 +7,7 @@ public class CashbackHackServiceTest {
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
 
-        assertEquals(service.remain(2000), 0);
+        assertEquals(service.remain(2_000), 0);
     }
 
     @org.junit.Test
@@ -15,5 +15,19 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
 
         assertEquals(service.remain(900), 100);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainJunit5() {
+        CashbackHackService service = new CashbackHackService();
+
+        org.junit.jupiter.api.Assertions.assertEquals(service.remain(2_000), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainForCashbackJunit5() {
+        CashbackHackService service = new CashbackHackService();
+
+        org.junit.jupiter.api.Assertions.assertEquals(service.remain(900), 100);
     }
 }
